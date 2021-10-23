259a92820bae94a3d7dbb443f21f67174d542bee
Mark McAvoy <mark@cartviper.com>
2021-10-23 07:14:39 -0400

    very simple react template used to output the tweets

--- React/tweet.js
+++ React/tweet.js
@@ -1,9 +1,31 @@
 import React from "react";
+import moment from "moment";
 
 const Tweet = ({tweet}) => {
 
+    const tweetAge = (createdDate) => {
+
+        var timeDiffMin = moment().diff(createdDate, "minutes");
+        var timeDiffHours = moment().diff(createdDate, 'hours');
+        var timeDiffDay = moment().diff(createdDate, 'days');
+
+        if (timeDiffDay >= 1)
+            return(<p>About {timeDiffDay} days ago</p>);
+        else if (timeDiffHours > 0)
+            return(<p>About {timeDiffHours} hours ago</p>);
+        else
+            return(<p>About {timeDiffMin} minutes ago</p>);
+
+    };
+
     return(<div>
-        TWEET
+        <div className="tweet-avatar">
+            <img src={tweet.ProfileImage} alt="" />
+        </div>
+        <div className="tweet-body">
+            <div dangerouslySetInnerHTML={{ __html: tweet.ProcessedText }} />
+            {tweetAge(tweet.Created)}
+        </div>
     </div>);
     
 };
